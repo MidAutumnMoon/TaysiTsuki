65ffb51e18f382008e90684b8deed09cc484a623
MidAutumnMoon <me@418.im>
2025-03-12 03:00:34 +0800

    everything: update
    up

--- packages/rust-analyzer/default.nix
+++ packages/rust-analyzer/default.nix
@@ -9,16 +9,16 @@
 rustTeapot.buildRustPackage rec {
 
     pname = "rust-analyzer";
-    version = "2025-03-10";
+    version = "2025-03-24";
 
     src = fetchFromGitHub {
         owner = "rust-lang";
         repo = pname;
         tag = version;
-        hash = "sha256-MmYdZyVwKa3YHzqoHdHw9/jZXkDE/5M3M+7363t4Pkc=";
+        hash = "sha256-Hs+FeeFB+YTKIW39/b2OKr1/Q+vgDnfMYM1g+sRgFCU=";
     };
 
-    cargoHash = "sha256-t2cA2JWV+l2cfTUrIpPojNmrElVty2JJAi8+XdxquTs=";
+    cargoHash = "sha256-cLSUCdc0q1P1z8STZ9GhNzT752ruFqyhnnhDzA6nb+o=";
     useFetchCargoVendor = true;
 
     doCheck = false;
