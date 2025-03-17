330bb994a717c0520ed0d1b02ff4702d9d6a92bd
MidAutumnMoon <me@418.im>
2025-03-18 07:56:17 +0800

    packages: add metacubexd

---
+++ packages/metacubexd/default.nix
@@ -0,0 +1,35 @@
+{
+    lib,
+    stdenvNoCC,
+    fetchurl,
+}:
+
+stdenvNoCC.mkDerivation ( drvSelf: {
+
+    pname = "metacubexd";
+    version = "1.186.0";
+
+    src = let
+        repo = "https://github.com/MetaCubeX/metacubexd";
+        dist = "compressed-dist.tgz";
+    in fetchurl {
+        url = "${repo}/releases/download/v${drvSelf.version}/${dist}";
+        hash = "sha256-GSnGqg/MKXbZaVVpSr/yi+UAWoqTrijqhMxjuZJj/DY=";
+    };
+
+    sourceRoot = ".";
+
+    installPhase = ''
+        mkdir -pv "$out"
+        cp -rv . "$out"
+    '';
+
+    postFixup = ''
+        rm "$out/env-vars"
+    '';
+
+    meta = {
+        license = lib.licenses.mit;
+    };
+
+} )
