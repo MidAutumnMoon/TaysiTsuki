43b0031f203b33eb0d9f3b4b5198bac09474c489
MidAutumnMoon <me@418.im>
2025-03-20 17:40:15 +0800

    nixos: don't delete man3

--- nixos/documentation/default.nix
+++ nixos/documentation/default.nix
@@ -127,8 +127,6 @@ in lib.mkIf ( cfg.enable ) {
             -mindepth 1 -maxdepth 1 \
             -not -name "man[1-8]" \
             -exec rm -r "{}" ";"
-
-        rm -r "$out/share/man/man3"
     '';
 
 } )
