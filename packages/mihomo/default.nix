5136abd6688b2ae3493b9d91e17e1ef1d2e8c670
MidAutumnMoon <me@418.im>
2025-04-01 23:08:15 +0800

    packages: custom build mihomo

--- packages/mihomo/default.nix
+++ packages/mihomo/default.nix
@@ -1,13 +1,45 @@
 {
-    mihomo,
-    ...
+    lib,
+    fetchFromGitHub,
+    buildGoModule,
 }:
 
-mihomo.override ( old: {
-    buildGoModule = attrs:
-        attrs // {
-            env = attrs.env or {} // { GOAMD64 = "v3"; };
-        }
-        |> old.buildGoModule
-    ;
-} )
+buildGoModule rec {
+
+    pname = "mihomo";
+    version = "1.19.4";
+
+    src = fetchFromGitHub {
+        owner = "MetaCubeX";
+        repo = "mihomo";
+        rev = "v${version}";
+        hash = "sha256-A/+BUnW7ge4y99W2rAUBAAqxO1L0M9oO0WSnLN1NnXQ=";
+    };
+
+    vendorHash = "sha256-VBDVtzI3GwxviLaAVUboHTtHaMQviiCUnB7ncgri+xc=";
+
+    excludedPackages = [ "./test" ];
+
+    env.GOAMD64 = "v3";
+    env.CGO_ENABLED = 0;
+
+    ldflags = [
+        "-s" "-w"
+        "-X github.com/metacubex/mihomo/constant.Version=${version}"
+    ];
+
+    tags = [
+        "no_fake_tcp"
+    ];
+
+    # network required
+    doCheck = false;
+
+    meta = with lib; {
+        description = "Rule-based tunnel in Go";
+        homepage = "https://github.com/MetaCubeX/mihomo/tree/Alpha";
+        license = licenses.gpl3Only;
+        mainProgram = "mihomo";
+    };
+
+}
