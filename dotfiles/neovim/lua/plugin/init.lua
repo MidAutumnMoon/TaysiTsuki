6c60752f91a3df1ffe9fe10036591197ffd4b96b
MidAutumnMoon <me@418.im>
2025-04-15 12:02:03 +0800

    nvim: remove the remaining of nvim.cmp

--- dotfiles/neovim/lua/plugin/init.lua
+++ dotfiles/neovim/lua/plugin/init.lua
@@ -231,22 +231,6 @@ local __plugins = {
     -- Completion & LSP
     --
 
-    -- {
-    --     "hrsh7th/nvim-cmp",
-    --     dependencies = {
-    --         "hrsh7th/cmp-nvim-lsp",
-    --         "hrsh7th/cmp-buffer",
-    --         "hrsh7th/cmp-nvim-lsp-signature-help",
-    --         "https://codeberg.org/FelipeLema/cmp-async-path",
-    --         "hrsh7th/cmp-cmdline",
-    --         "saadparwaiz1/cmp_luasnip",
-    --         "onsails/lspkind.nvim",
-    --     },
-    --     config = function()
-    --         require "plugin.cmp"
-    --     end
-    -- },
-
     {
         "saghen/blink.cmp",
         version = "*",
