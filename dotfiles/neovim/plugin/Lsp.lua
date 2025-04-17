66c3d8374f18de138ff98890dd7eaa30e1704fe7
MidAutumnMoon <me@418.im>
2025-04-17 14:16:27 +0800

    nvim: remove custom mapping of open_float
    It's mapped to <C-w>d by default

--- dotfiles/neovim/plugin/Lsp.lua
+++ dotfiles/neovim/plugin/Lsp.lua
@@ -30,7 +30,6 @@ vim.lsp.enable {
 vim.api.nvim_create_autocmd( 'LspAttach', {
     callback = function( args )
         local keyopts = { buffer = args.buf, silent = true }
-        vim.keymap.set( "n", "D", vim.diagnostic.open_float, keyopts )
         vim.keymap.set( "n", "<F2>", vim.lsp.buf.rename, keyopts )
     end
 } )
