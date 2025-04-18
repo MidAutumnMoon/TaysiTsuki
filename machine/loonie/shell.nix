226de07be8f5e9ebb3c65becc0fc9dabc9ccb305
MidAutumnMoon <me@418.im>
2025-04-18 16:27:18 +0800

    nixos: move all fish config from home-manager

--- machine/loonie/shell.nix
+++ machine/loonie/shell.nix
@@ -18,7 +18,14 @@
     '';
 
     programs.fish.interactiveInit = /*fish*/ ''
-        ${builtins.readFile ./tide.fish}
+        ${builtins.readFile ./fish/tide.fish}
+        ${builtins.readFile ./fish/git-abbr.fish}
+
+        # Tell Windows Terminal to open new tabs with the same CWD
+        function __windows_terminal --on-variable PWD
+            set -q WT_SESSION
+            and printf "\e]9;9;%s\e\\" ( wslpath -w "$PWD" )
+        end
     '';
 
     environment.shellAliases = {
@@ -26,6 +33,12 @@
         "ldd" = "libtree";
         "sys" = "systemctl";
         "ca" = "cargo";
+        "g" = "git";
+        "n" = "nix";
+        ".ns" = "nh os switch";
+        ".nt" = "nh os test";
+        ".nb" = "nh os build";
+        ".npl" = "nh os repl";
     };
 
     programs.fish.functions = {
--- nixos/shell/default.nix
+++ nixos/shell/default.nix
@@ -1,11 +1,16 @@
+{ lib, ... }:
+
 {
 
     imports = [
+        ./implementation-fish
         ./fish.nix
     ];
 
     environment.shellAliases = {
         "ll" = null;
     };
+
+    programs.command-not-found.enable = lib.mkForce false;
 
 }
