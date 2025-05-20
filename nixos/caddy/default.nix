066d2ab4e401101b0f5b970115c67886f6dca729
MidAutumnMoon <me@418.im>
2025-05-21 04:42:59 +0800

    nixos/caddy: hardening

--- nixos/caddy/default.nix
+++ nixos/caddy/default.nix
@@ -4,4 +4,32 @@
 
     services.caddy.package = pkgs.tsuki.caddy;
 
+    systemd.services.caddy.serviceConfig = {
+        RemoveIPC = true;
+        NoNewPrivileges = true;
+        CapabilityBoundingSet = "";
+        ProtectClock = true;
+        ProtectKernelLogs = true;
+        ProtectControlGroups = true;
+        ProtectKernelModules = true;
+        ProtectHostname = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        SystemCallArchitectures = "native";
+        MemoryDenyWriteExecute = true;
+        RestrictNamespaces = true;
+        RestrictSUIDSGID = true;
+        RestrictRealtime = true;
+        LockPersonality = true;
+        SystemCallFilter = "@system-service";
+        ProtectProc = "invisible";
+        ProcSubset = true;
+        PrivateMounts = true;
+        RestrictAddressFamilies = [
+            "AF_UNIX"
+            "AF_INET"
+            "AF_INET6"
+        ];
+    };
+
 }
