9d96296dfac379059958dab4b498e33aca2b2470
MidAutumnMoon <me@418.im>
2025-05-31 22:40:13 +0800

    nixos/caddy: add proxy related envvars

--- nixos/caddy/default.nix
+++ nixos/caddy/default.nix
@@ -1,35 +1,48 @@
 { pkgs, config, lib, ... }:
 
-lib.mkIf config.services.caddy.enable {
+let
+
+    caddyCfg = config.services.caddy;
+
+    proxyCfg = config.networking.proxy;
+
+in
+
+lib.mkIf caddyCfg.enable {
 
     services.caddy.package = pkgs.tsuki.caddy;
 
-    systemd.services.caddy.serviceConfig = {
-        RemoveIPC = true;
-        NoNewPrivileges = true;
-        CapabilityBoundingSet = "";
-        ProtectClock = true;
-        ProtectKernelLogs = true;
-        ProtectControlGroups = true;
-        ProtectKernelModules = true;
-        ProtectHostname = true;
-        ProtectKernelTunables = true;
-        ProtectSystem = "strict";
-        SystemCallArchitectures = "native";
-        MemoryDenyWriteExecute = true;
-        RestrictNamespaces = true;
-        RestrictSUIDSGID = true;
-        RestrictRealtime = true;
-        LockPersonality = true;
-        SystemCallFilter = "@system-service";
-        ProtectProc = "invisible";
-        ProcSubset = "pid";
-        PrivateMounts = true;
-        RestrictAddressFamilies = [
-            "AF_UNIX"
-            "AF_INET"
-            "AF_INET6"
-        ];
+    systemd.services.caddy = {
+        serviceConfig = {
+            RemoveIPC = true;
+            NoNewPrivileges = true;
+            CapabilityBoundingSet = "";
+            ProtectClock = true;
+            ProtectKernelLogs = true;
+            ProtectControlGroups = true;
+            ProtectKernelModules = true;
+            ProtectHostname = true;
+            ProtectKernelTunables = true;
+            ProtectSystem = "strict";
+            SystemCallArchitectures = "native";
+            MemoryDenyWriteExecute = true;
+            RestrictNamespaces = true;
+            RestrictSUIDSGID = true;
+            RestrictRealtime = true;
+            LockPersonality = true;
+            SystemCallFilter = "@system-service";
+            ProtectProc = "invisible";
+            ProcSubset = "pid";
+            PrivateMounts = true;
+            RestrictAddressFamilies = [
+                "AF_UNIX"
+                "AF_INET"
+                "AF_INET6"
+            ];
+        };
+        environment =
+            with proxyCfg;
+            lib.mkIf ( envVars != {} ) envVars;
     };
 
 }
