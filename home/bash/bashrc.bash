0fdbb65a6711c53f1602a1974b7ea92cef676391
MidAutumnMoon <me@418.im>
2025-06-04 18:27:40 +0800

    home/bash: only launch fish if interactive

--- home/bash/bashrc.bash
+++ home/bash/bashrc.bash
@@ -1,45 +1,30 @@
-# 1)
-#
-# Grab the status of Bash and launch fish in
-# login or interactive mode accordingly.
+# Only launchs fish if it's interactive shell
+if [[ $- == *i* ]]; then
+    # Walk through parent processes up until PID 1
+    # to check if any of them is "fish". If such case
+    # is encountered, this Bash process is probably
+    # launched using nix shell or develop, in this case
+    # don't start a new fish shell.
 
-declare fish_params
+    declare pid_on_hand="$$"
+    declare parents_have_fish="false"
 
-if shopt -q login_shell; then
-    fish_params="--login"
-elif [[ $- == *i* ]]; then
-    fish_params="--interactive"
-fi
+    while [[ "$pid_on_hand" -ne 1 ]]; do
+        # Get the command name
+        cmd=$( ps -p "$pid_on_hand" -o cmd= )
 
-# 2)
-#
-# Walk through parent processes up until PID 1
-# to check if any of them is "fish". If such case
-# is encountered, this Bash process is probably
-# launched using nix shell or develop, in this case
-# don't start a new fish shell.
+        if [[ "$cmd" == *fish* ]]; then
+            parents_have_fish="true"
+            break
+        fi
 
-declare pid_on_hand="$$"
-declare parents_have_fish="false"
+        parent_pid=$( ps -p "$pid_on_hand" -o ppid= )
+        # Remove whitespaces from the idiot output
+        pid_on_hand="${parent_pid//[[:space:]]}"
+    done
 
-while [[ "$pid_on_hand" -ne 1 ]]; do
-    # Get the command name
-    cmd=$( ps -p "$pid_on_hand" -o cmd= )
-
-    if [[ "$cmd" == *fish* ]]; then
-        parents_have_fish="true"
-        break
+    if [[ "$parents_have_fish" = "false" ]]; then
+        exec fish --interactive
     fi
-
-    parent_pid=$( ps -p "$pid_on_hand" -o ppid= )
-    # Remove whitespaces from the idiot output
-    pid_on_hand="${parent_pid//[[:space:]]}"
-done
-
-# 3)
-#
-# Launch fish (or not)
-
-if [[ "$parents_have_fish" = "false" ]]; then
-    exec fish "$fish_params"
 fi
+
