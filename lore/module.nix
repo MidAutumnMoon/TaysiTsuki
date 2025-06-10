4ad54f70dc2684f57b944a5c9f4e984662c768af
MidAutumnMoon <me@418.im>
2025-06-10 16:23:39 +0800

    lore: move pubkeys to shared.json

--- lore/module.nix
+++ lore/module.nix
@@ -22,9 +22,7 @@ in
     lore.tsukiObservatory =
         "${config.users.users.teapot.home}/TaysiTsuki";
 
-    lore.pubkeys = {
-        teapot = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEyX4qdUuwPEqQa+QaR8/0MubpfB9rHbpGAH+yEM9kxM me@418.im";
-    };
+    lore.pubkeys = sharedInfra.pubkeys;
 
     lore.ports = {
         proxyPort = 7890;
