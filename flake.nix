5c1c63cba0ce19ffdd1991efe01c98eab62ee436
MidAutumnMoon <me@418.im>
2025-06-10 16:16:58 +0800

    flake: remove nixos-wsl

--- flake.nix
+++ flake.nix
@@ -19,12 +19,6 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        nixos-wsl = {
-            url = "github:nix-community/NixOS-WSL";
-            inputs.nixpkgs.follows = "nixpkgs";
-            inputs.flake-compat.follows = "empty";
-        };
-
         # Some packages
 
         colmena = {
