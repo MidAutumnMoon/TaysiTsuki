1b849f778b4938f6e948a6ead41ad48cb20a3338
MidAutumnMoon <me@418.im>
2025-06-11 14:27:43 +0800

    nvim: tweak fuzzy sorts

--- dotfiles/neovim/lua/plugin/blink.lua
+++ dotfiles/neovim/lua/plugin/blink.lua
@@ -79,14 +79,42 @@ option.snippets = {
 option.fuzzy = {
     implementation = "rust",
     sorts = {
-        "exact", "score", "sort_text",
-    }
+        -- sort items with auto-imports below imported ones
+        function( a, b )
+            local function has_imports( val )
+                local data = val.data or {}
+                local imports = data.imports
+                return imports ~= nil and #imports > 0
+            end
+
+            local a_score = a.score
+            local b_score = b.score
+            local diff = math.abs( a_score - b_score ) / ( a_score + b_score )
+            local a_auto = has_imports( a )
+            local b_auto = has_imports( b )
+
+            -- whoever is auto import got sorted below another
+            if a_auto and not b_auto then
+                -- b comes before a
+                if diff < 0.2 then return false end
+            elseif not a_auto and b_auto then
+                -- a comes before b
+                -- return true
+                if diff < 0.2 then return true end
+            end
+        end,
+        "score",
+        "sort_text",
+    },
+    max_typos = function( kw )
+        return 2
+    end,
 }
 
-option.sources = {}
-
-option.sources.providers = {
-    lsp = { timeout_ms = 1000, },
+option.sources = {
+    providers = {
+        lsp = { timeout_ms = 1000, },
+    }
 }
 
 blink.setup( option )
