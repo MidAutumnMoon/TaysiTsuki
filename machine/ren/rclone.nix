bf8e4ec5781964e819a90641771e575b0c457e48
MidAutumnMoon <me@418.im>
2025-06-20 13:24:15 +0800

    ren: fix rclone 1.70 mount

--- machine/ren/rclone.nix
+++ machine/ren/rclone.nix
@@ -1,11 +1,15 @@
 { config, lib, pkgs, ... }:
 
+let
+
+    inherit ( config.sops ) secrets;
+    inherit ( config.users.users ) teapot;
+
+in
+
 {
 
-    systemd.tmpfiles.rules = let
-        inherit ( config.sops ) secrets;
-        inherit ( config.users.users ) teapot;
-    in [
+    systemd.tmpfiles.rules = [
         "C /etc/rclone.conf - - - - ${secrets."conf--rclone".path}"
         #"z /etc/rclone.conf 0440 ${teapot.name} ${teapot.group} - -"
         "z /etc/rclone.conf 0440 ${teapot.name} - - -"
@@ -21,14 +25,18 @@
         serviceConfig.ExecStartPre = ''
             mkdir -pv /mnt/%i
         '';
+        # N.B. log to /dev/null because 1.70 regression. Either --log-systemd
+        # or not setting log file will cause rclone to hang until service
+        # timeout and fail. /dev/stdout is not found when set log-file to it,
+        # this might be a hint of the bug?
         serviceConfig.ExecStart = /* bash */ ''
             ${lib.getExe pkgs.rclone} mount \
                 --config "/etc/rclone.conf" \
-                --log-level "INFO" \
-                --log-systemd \
+                --log-level "DEBUG" \
+                --log-file "/dev/null" \
                 --human-readable \
                 --use-mmap \
-                --cache-dir "/%T/rclone_cache_%i" \
+                --cache-dir "%T/rclone_%i" \
                 --vfs-cache-mode "full" \
                 --vfs-cache-max-size "2G" \
                 --vfs-cache-max-age "10m" \
@@ -38,11 +46,13 @@
                 --multi-thread-streams "4" \
                 --umask "022" \
                 --allow-other \
-                --disable-http2 \
                 %i: /mnt/%i
         '';
-        serviceConfig.ExecStop = "fusermount -u /mnt/Rclone/%i";
-        environment = config.networking.proxy.envVars;
+        # --disable-http2 \
+        # serviceConfig.ExecStop = "fusermount -u /mnt/%i";
+        environment =
+            config.networking.proxy.envVars
+            // { GODEBUG = "netdns=go"; };
     };
 
     systemd.targets."rclone-mounts" = {
