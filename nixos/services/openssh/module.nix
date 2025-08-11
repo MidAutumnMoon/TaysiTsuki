9283c88909bc1f9d58551cbaa7b3b7a43eb26ce7
MidAutumnMoon <me@418.im>
2025-08-11 22:20:54 +0800

    nixos/ssh: remove sntrup* from KexAlgorithms

--- nixos/services/openssh/module.nix
+++ nixos/services/openssh/module.nix
@@ -9,11 +9,7 @@
         Ciphers = [ "chacha20-poly1305@openssh.com" ];
         HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519";
         PubkeyAcceptedAlgorithms = HostKeyAlgorithms;
-        KexAlgorithms = [
-            "mlkem768x25519-sha256"
-            # remove once ssh on stable Debian reaches >= 9.9
-            "sntrup761x25519-sha512@openssh.com"
-        ];
+        KexAlgorithms = [ "mlkem768x25519-sha256" ];
         Macs = [ "hmac-sha2-512-etm@openssh.com" ];
     };
 
