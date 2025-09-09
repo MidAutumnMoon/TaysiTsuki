3848dc3907827baf6ca60d448b1592a1282c4ebc
MidAutumnMoon <me@418.im>
2025-09-09 16:28:04 +0800

    nixos: tweak network sysctl

--- nixos/networking/module.nix
+++ nixos/networking/module.nix
@@ -14,15 +14,13 @@
 
 
     boot.kernel.sysctl = {
-        "net.core.default_qdisc" = "fq";
+        "net.core.default_qdisc" = "fq_pie";
         "net.ipv4.tcp_congestion_control" = "bbr";
         "net.ipv4.tcp_notsent_lowat" = 16384;
         "net.ipv4.tcp_slow_start_after_idle" = 0;
         "net.ipv4.tcp_no_metrics_save" = 0;
-        "net.core.rmem_max" = 67108864;
-        "net.core.wmem_max" = 67108864;
         "net.ipv4.tcp_mtu_probing" = 1;
-        "net.core.somaxconn" = 16384;
+        "net.ipv4.tcp_frto" = 0;
     };
 
 }
