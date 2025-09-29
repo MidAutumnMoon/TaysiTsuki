382d8f1b01714a1373d163f1b3b9df57df41a9f1
MidAutumnMoon <me@418.im>
2025-09-30 01:31:10 +0800

    package: remove build-driver

--- packages/__tools/package.nix
+++ packages/__tools/package.nix
@@ -16,10 +16,6 @@ in {
         shebangProgram = pkgs.tsuki.ruby.with_preferred_gems;
     };
 
-    build-driver = self.rubyMiner {
-        path = ./build-driver.rb;
-    };
-
     nix-update-driver = self.rubyMiner {
         path = ./nix-update-driver.rb;
         runtimeDeps = with pkgs; [ nix-update ];
