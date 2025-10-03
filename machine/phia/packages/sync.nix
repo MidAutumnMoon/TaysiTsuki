dda6b5712d4f3c8afe512af163da35a1a1c4ce31
MidAutumnMoon <me@418.im>
2025-10-03 11:55:58 +0800

    remove usage of reinbow

--- machine/phia/packages/sync.nix
+++ machine/phia/packages/sync.nix
@@ -4,18 +4,14 @@
     myRclone,
 }:
 
-
 tsuki.writers.writeRubyBin ",sync" {} /*ruby*/ ''
     # frozen_string_literal: true
 
     require "shellwords"
 
-    require "reinbow"
-    using Reinbow
-
     DIRECTION = ARGV.shift
 
-    abort %(Expect an sync direction of either "up" or "down").red \
+    abort %(Expect an sync direction of either "up" or "down") \
         unless DIRECTION in "up" | "down"
 
     # N.B. Hardcoded paths!
@@ -23,10 +19,10 @@ tsuki.writers.writeRubyBin ",sync" {} /*ruby*/ ''
     RCLONE_REMOTE = "Box"
     POOL_DIR = "/srv/pool"
 
-    abort %("rclone.conf" is not at expected location).red \
+    abort %("rclone.conf" is not at expected location) \
         unless File.file? "/etc/rclone.conf"
 
-    abort %("#{POOL_DIR}" not at expected location).red \
+    abort %("#{POOL_DIR}" not at expected location) \
         unless File.directory? POOL_DIR
 
     EXCLUDE_PATTERNS =
@@ -46,7 +42,7 @@ tsuki.writers.writeRubyBin ",sync" {} /*ruby*/ ''
             [ "#{RCLONE_REMOTE}:", POOL_DIR ]
         end
 
-    system <<~SH or abort "Failed to run rclone!".red
+    system <<~SH or abort "Failed to run rclone!"
         "${lib.getExe myRclone}" sync --refresh-times \
             #{EXCLUDE_PATTERNS.shelljoin} \
             #{RCLONE_DIR_ARGS.shelljoin}
