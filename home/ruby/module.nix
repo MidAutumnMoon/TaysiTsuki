279c8f3b7505faf74e316c835a4ac6f7eff5b688
MidAutumnMoon <me@418.im>
2025-10-03 12:06:54 +0800

    home: remove custom ruby

--- home/ruby/module.nix
+++ home/ruby/module.nix
@@ -3,18 +3,9 @@
 {
 
     packages = with pkgs; [
-        tsuki.ruby.with_preferred_gems
-        tsuki.ruby.rubocop
+        ruby_3_4
+        rubocop
     ];
-
-    xdg_config."irb/irbrc".text = /*ruby*/ ''
-        begin
-            require "amazing_print"
-            AmazingPrint.irb!
-        rescue LoadError
-            warn "Can't load amazing_print"
-        end
-    '';
 
     xdg_config."rubocop/config.yml".src =
         "${tsukiObservatory}/.rubocop.yml";
