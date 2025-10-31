f59228d303ffacc90822af5dd10187c7c9e7f1c1
MidAutumnMoon <me@418.im>
2025-10-31 13:03:33 +0800

    home/swayimg: build from master

--- home/swayimg/module.nix
+++ home/swayimg/module.nix
@@ -1,9 +1,24 @@
 { dots, pkgs, ... }:
 
+let
+
+    # wait for v4.6+
+    # scroll at cursor's position
+    newSwayimg = pkgs.swayimg.overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+            owner = "artemsen";
+            repo = "swayimg";
+            rev = "ac21e8c9a8385e49985fca6f2f03469231db0b75";
+            hash = "sha256-+DOPX9wrS3367u6STbrOWzUcZR1T0zz8N7jHNh9K3tc=";
+        };
+    };
+
+in
+
 {
 
     packages = with pkgs; [
-        swayimg
+        newSwayimg
     ];
 
     xdg_config."swayimg/config".src = dots.get "swayimg/swayimgrc.ini";
