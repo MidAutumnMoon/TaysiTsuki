7ff0a1324819aade17fc0588d061dd2283180157
MidAutumnMoon <me@418.im>
2025-11-06 21:20:54 +0800

    ren: massively simplify dns server config

--- machine/ren/networking/dns/module.nix
+++ machine/ren/networking/dns/module.nix
@@ -1,21 +1,25 @@
-{ lib, config, pkgs, flakes, ... }:
+{ lib, config, pkgs, ... }:
 
 let
 
-    # N.B.
-    # For security reasons, loopback addresses can't be used for dnat by default.
-    # Ref: `net.ipv4.conf.interface.route_localnet`
-    fakeAddr = "192.168.20.10";
-    fakeAddrV6 = "fd7a:1ca5:11bf::1";
-
-    inherit ( config )
-        lore
-    ;
-
-    inherit ( lore )
-        ports
-        apps
-    ;
+    localDomains =
+        let
+            # TODO: handle conflict names?
+            localApps = with config.lore.apps; homelab // tailnet;
+            try = app: attr: (builtins.tryEval (app.${attr})).success;
+            fmt = app:
+                if try app "ip" then "A ${app.ip}" else
+                if try app "ip6" then "AAAA ${app.ip6}" else
+                if try app "cname" then "CNAME ${app.cname}"
+                else builtins.throw "[BUG] invalid app ${app.fqdn}";
+            localData = localApps
+                |> lib.attrValues
+                |> map (app: ''${app.fqdn}. IN ${fmt app}'');
+        in pkgs.writeText "local-domains" ''
+            $TTL 69
+            example.com. IN SOA a b (1 7200 3600 3600 3600)
+            ${lib.join "\n" localData}
+        '';
 
 in
 
@@ -32,90 +36,58 @@ in
     services.coredns = {
         enable = true;
         package = pkgs.tsuki.coredns;
-        config =
-            let
-                hostSystem = pkgs.stdenv.hostPlatform.system;
-                inherit (flakes.dns.util.${hostSystem}) writeZone;
-                appToZone = app:
-                    lore.utils.appToDnsRecords app |> writeZone "418.im";
-            in ''
-                .:53 {
-                    bind ${fakeAddr} ${fakeAddrV6}
-                    log
-                    # N.B. multiple directives cause later one
-                    # to override previous ones
-                    file ${appToZone <| apps.homelab // apps.tailnet} {
-                        # at least v1.12.2
-                        fallthrough
-                    }
-                    forward local /etc/resolv.conf
-                    forward . [::1]:${toString ports.dnscryptLocal}
-                    cache 60 {
-                        disable success local
-                        disable denial local
-                    }
-                }
-            '';
     };
+
+    services.coredns.config =
+        let
+            avahi2dnsPort = toString config.services.avahi2dns.port;
+            dnscryptPort = toString config.lore.ports.dnscryptLocal;
+        in ''
+            (the_conf) {
+                log
+                file ${localDomains} {
+                    # at least v1.12.2
+                    fallthrough
+                }
+                forward local 127.0.0.1:${avahi2dnsPort}
+                forward . [::1]:${dnscryptPort}
+                cache 60 {
+                    disable success local
+                    disable denial local
+                }
+            }
+            .:53 {
+                bind enp3s0
+                import the_conf
+            }
+            .:53 {
+                bind 127.0.0.1 ::1
+                import the_conf
+            }
+        '';
 
     # dnscrypt-proxy resolves internet names
     services.dnscrypt-proxy = {
         enable = true;
         upstreamDefaults = false;
+        package = pkgs.tsuki.dnscrypt;
         settings = rec {
-            # Global Options
+            listen_addresses = [
+                "[::1]:${toString config.lore.ports.dnscryptLocal}"
+            ];
             server_names = lib.attrNames static;
-            listen_addresses = [ "[::]:${toString ports.dnscryptLocal}" ];
-            bootstrap_resolvers = [ "223.5.5.5:53" ];
-            netprobe_address = "8.8.8.8:53";
-            # cache handled by coredns
-            cache = false;
-            ignore_system_dns = true;
-            # Static Servers
-            static."doh-pub".stamp = "sdns://AgcAAAAAAAAAAAAHZG9oLnB1YgovZG5zLXF1ZXJ5";
-            static."alidns".stamp = "sdns://AgcAAAAAAAAAAAAOZG5zLmFsaWRucy5jb20KL2Rucy1xdWVyeQ";
-            # Blocklist
-            blocked_names = {
-                blocked_names_file = pkgs.tsuki.adblocklist;
-                log_file = "/dev/stdout";
+            static = {
+                "doh-pub".stamp =
+                    "sdns://AgcAAAAAAAAAAAAHZG9oLnB1YgovZG5zLXF1ZXJ5";
+                "alidns".stamp =
+                    "sdns://AgcAAAAAAAAAAAAOZG5zLmFsaWRucy5jb20KL2Rucy1xdWVyeQ";
             };
-            # Logging
-            query_log.file = "/dev/stdout";
-            nx_log.file = "/dev/stdout";
-            monitoring_ui.enabled = false;
+            bootstrap_resolvers = [ "223.5.5.5:53" ];
+            netprobe_address = "223.5.5.5:53";
+            cache = false;
+            blocked_names.blocked_names_file =
+                "${pkgs.tsuki.adblocklist}/blocklist-dnscrypt.txt";
         };
-    };
-
-    networking.nftables.tables."dns-nat" =
-        let
-            interfaces = [ "enp3s0" ]
-                |> lib.concatStringsSep ", "
-                |> ( it: "{ ${it} }" );
-        in {
-            family = "inet";
-            content = ''
-                chain pre {
-                    type nat hook prerouting priority dstnat; policy accept
-                    iifname ${interfaces} meta l4proto { tcp, udp } th dport 53 \
-                        dnat ip to ${fakeAddr}
-                    iifname ${interfaces} meta l4proto { tcp, udp } th dport 53 \
-                        dnat ip6 to ${fakeAddrV6}
-                }
-            '';
-        };
-
-    networking = {
-        interfaces."lo" = {
-            ipv4.addresses = [
-                { address = fakeAddr; prefixLength = 32; }
-            ];
-            ipv6.addresses = [
-                { address = fakeAddrV6; prefixLength = 128; }
-            ];
-        };
-        nameservers = [ fakeAddr fakeAddrV6 ];
     };
 
 }
-
-# vim: nowrap:
