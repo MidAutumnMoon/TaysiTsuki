2ca58a3d8bd1142082db18557403658820967e5e
MidAutumnMoon <me@418.im>
2025-11-23 13:09:54 +0800

    home/swayimg: remove override

--- home/swayimg/module.nix
+++ home/swayimg/module.nix
@@ -1,24 +1,9 @@
 { dots, pkgs, ... }:
 
-let
-
-    # wait for v4.6+
-    # scroll at cursor's position
-    newSwayimg = pkgs.swayimg.overrideAttrs {
-        src = pkgs.fetchFromGitHub {
-            owner = "artemsen";
-            repo = "swayimg";
-            rev = "fc562799131d3e962a6c14e4a78e8c820953c6e3";
-            hash = "sha256-Q5TqY5UgSGZVVoARcz1ig0UkZBKgRoT9Ndvg/f1T38Q=";
-        };
-    };
-
-in
-
 {
 
     packages = with pkgs; [
-        newSwayimg
+        swayimg
     ];
 
     xdg_config."swayimg/config".src = dots.get "swayimg/swayimgrc.ini";
