a5c12c4e840c99fb8c477b1b1edd235b8eceda94
MidAutumnMoon <me@418.im>
2026-01-03 13:10:10 +0800

    packages: remove node 22 override

--- packages/default.nix
+++ packages/default.nix
@@ -40,16 +40,6 @@ in rec {
         sops-install-secrets
     ;
 
-    bluesky-pds = prev.bluesky-pds.override {
-        nodejs = prev.nodejs_22;
-    };
-
-    sillytavern = prev.sillytavern.override {
-        buildNpmPackage = prev.buildNpmPackage.override {
-            nodejs = prev.nodejs_22;
-        };
-    };
-
     # tangled = {
     #     inherit (pkgsFrom "tangled")
     #         knot
