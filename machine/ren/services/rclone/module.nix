bd45f1aae3388e8a90463df41a40bc2b6a3c43e5
MidAutumnMoon <me@418.im>
2026-01-04 08:36:57 +0800

    ren: fix rclone?

--- machine/ren/services/rclone/module.nix
+++ machine/ren/services/rclone/module.nix
@@ -9,32 +9,31 @@
         serviceConfig = {
             Type = "notify";
             RuntimeDirectory = "rclone";
-            ExecSearchPath = "${lib.getBin pkgs.coreutils}/bin";
         };
         serviceConfig.ExecStartPre = ''
-            mkdir -pv /mnt/%i
+            ${lib.getExe' pkgs.coreutils "mkdir"} -pv /mnt/%i
         '';
-        serviceConfig.ExecStart = /* bash */ ''
-            ${lib.getExe pkgs.rclone} mount \
-                --config "/etc/rclone.conf" \
-                --log-level "DEBUG" \
-                --log-systemd \
-                --human-readable \
-                --use-mmap \
-                --cache-dir "%T/rclone_%i" \
-                --vfs-cache-mode "full" \
-                --vfs-cache-max-size "2G" \
-                --vfs-cache-max-age "10m" \
-                --dir-cache-time "2m" \
-                --poll-interval "1m" \
-                --multi-thread-cutoff "128M" \
-                --multi-thread-streams "8" \
-                --umask "022" \
-                --disable-http2 \
-                --allow-other \
-                %i: /mnt/%i
-        '';
-        # serviceConfig.ExecStop = "fusermount -u /mnt/%i";
+        serviceConfig.ExecStart = lib.concatStringsSep " " [
+            "${lib.getExe pkgs.rclone}"
+            "mount"
+            "--config" "/etc/rclone.conf"
+            "--log-level" "DEBUG"
+            "--log-systemd"
+            "--human-readable"
+            "--use-mmap"
+            "--cache-dir" "%T/rclone_%i"
+            "--vfs-cache-mode" "full"
+            "--vfs-cache-max-size" "2G"
+            "--vfs-cache-max-age" "10m"
+            "--dir-cache-time" "2m"
+            "--poll-interval" "1m"
+            "--multi-thread-cutoff" "128M"
+            "--multi-thread-streams" "8"
+            "--umask" "022"
+            # "--disable-http2"
+            "--allow-other"
+            "%i: /mnt/%i"
+        ];
         environment =
             config.networking.proxy.envVars
             // { GODEBUG = "netdns=go"; };
