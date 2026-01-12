b851c05e52a0d91b1aadabef1c70a175e41c6f56
MidAutumnMoon <me@418.im>
2026-01-12 17:29:25 +0800

    sing-box: 1.13 beta 4

--- packages/sing-box/package.nix
+++ packages/sing-box/package.nix
@@ -9,14 +9,14 @@
 stdenv.mkDerivation (drvSelf: {
 
     pname = "sing-box";
-    version = "1.13.0-beta.2";
+    version = "1.13.0-beta.4";
 
     src = tsuki.fetchGitHubRelease {
         owner = "SagerNet";
         repo = "sing-box";
         tag = "v${drvSelf.version}";
         file = "sing-box-${drvSelf.version}-linux-amd64-musl.tar.gz";
-        hash = "sha256-ifRhuneKKefGFyHBvCF51oQY9mPDH88xIiT9zoETXck=";
+        hash = "sha256-jePdrerrWU7jds4doNf5LUJIlto+jRbBsD2yV6Kil3E=";
     };
 
     nativeBuildInputs = [
