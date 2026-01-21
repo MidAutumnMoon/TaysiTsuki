a9c2d02121ac4d2adc0dc53309c89ddbe7f4a35d
MidAutumnMoon <me@418.im>
2026-01-21 16:37:31 +0800

    home/niri: fix icon and cursors

--- machine/ren/programs/niri/module.nix
+++ machine/ren/programs/niri/module.nix
@@ -3,7 +3,13 @@
 {
     programs.niri.enable = true;
 
-    environment.systemPackages = [];
+    environment.systemPackages = with pkgs; [
+        kdePackages.qt6ct
+        kdePackages.breeze
+        kdePackages.breeze-icons
+        nwg-look
+        adwaita-icon-theme
+    ];
 
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
