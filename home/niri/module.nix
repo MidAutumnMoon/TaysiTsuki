505ce6959c70b36c9a63651cb07874ed535bd667
MidAutumnMoon <me@418.im>
2026-01-21 17:40:57 +0800

    home/niri: dolphin

--- home/niri/module.nix
+++ home/niri/module.nix
@@ -2,11 +2,24 @@
 
 {
 
+    # ref: https://gist.github.com/linhusp/05f8f7e0af3fa0fbb944dec17a75aa78
     packages = with pkgs; [
         kdePackages.dolphin
         kdePackages.dolphin-plugins
         kdePackages.ffmpegthumbs
+        kdePackages.kded
+        kdePackages.qt6ct
+        kdePackages.kservice
+        ffmpegthumbnailer
     ];
+
+    envvars = {
+        XDG_MENU_PREFIX = "plasma-";
+        QT_QPA_PLATFORM = "wayland";
+        QT_QPA_PLATFORMTHEME = "qt6ct";
+        QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
+        GTK_DECORATION_LAYOUT = "";
+    };
 
     xdg_config."niri".src = dots.get "niri";
 
--- machine/ren/programs/niri/module.nix
+++ machine/ren/programs/niri/module.nix
@@ -1,12 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
     programs.niri.enable = true;
+    programs.niri.useNautilus = false;
 
     environment.systemPackages = with pkgs; [
-        kdePackages.qt6ct
         kdePackages.breeze
         kdePackages.breeze-icons
+        kdePackages.breeze-gtk
         nwg-look
         adwaita-icon-theme
     ];
@@ -14,4 +15,15 @@
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
     hardware.i2c.enable = true;
+
+    xdg.portal = {
+        extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+        config.niri = {
+            "org.freedesktop.impl.portal.FileChooser" = lib.mkForce "kde";
+        };
+    };
+
+    # ref: https://github.com/NixOS/nixpkgs/issues/409986
+    environment.etc."xdg/menus/applications.menu".source =
+        "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 }
