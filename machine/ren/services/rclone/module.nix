cfa28dd04c0f19f16bcfdfaedc9f24475268a6c6
MidAutumnMoon <me@418.im>
2026-01-23 19:38:14 +0800

    ren: fix rclone mount

--- machine/ren/services/rclone/module.nix
+++ machine/ren/services/rclone/module.nix
@@ -2,50 +2,45 @@
 
 {
 
-    # systemd.services."rclone@" = {
-    #     description = "rclone mount for remote %i";
-    #     requires = [ "sops-install-secrets.service" ];
-    #     after = [ "sops-install-secrets.service" ];
-    #     serviceConfig = {
-    #         Type = "notify";
-    #         RuntimeDirectory = "rclone";
-    #     };
-    #     serviceConfig.ExecStartPre = ''
-    #         ${lib.getExe' pkgs.coreutils "mkdir"} -pv /mnt/%i
-    #     '';
-    #     serviceConfig.ExecStart = lib.concatStringsSep " " [
-    #         "${lib.getExe pkgs.rclone}"
-    #         "mount"
-    #         "--config" "/etc/rclone.conf"
-    #         "--log-level" "DEBUG"
-    #         "--log-systemd"
-    #         "--human-readable"
-    #         "--use-mmap"
-    #         "--cache-dir" "%T/rclone_%i"
-    #         "--vfs-cache-mode" "full"
-    #         "--vfs-cache-max-size" "2G"
-    #         "--vfs-cache-max-age" "10m"
-    #         "--dir-cache-time" "2m"
-    #         "--poll-interval" "1m"
-    #         "--multi-thread-cutoff" "128M"
-    #         "--multi-thread-streams" "8"
-    #         "--umask" "022"
-    #         # "--disable-http2"
-    #         "--allow-other"
-    #         "%i: /mnt/%i"
-    #     ];
-    #     environment =
-    #         config.networking.proxy.envVars
-    #         // { GODEBUG = "netdns=go"; };
-    #     wantedBy = [ "multi-user.target" ];
-    # };
-
-    # systemd.targets."rclone-mounts" = {
-    #     wantedBy = [ "multi-user.target" ];
-    #     wants = [
-    #         "network.target"
-    #         "rclone@Box.service"
-    #     ];
-    # };
+    systemd.services."rclone-box" = {
+        description = "rclone mount for storage box";
+        requires = [
+            "sops-install-secrets.service"
+        ];
+        after = [
+            "sops-install-secrets.service"
+        ];
+        serviceConfig = {
+            Type = "notify";
+            RuntimeDirectory = "rclone";
+        };
+        serviceConfig.ExecStartPre =
+            "${lib.getExe' pkgs.coreutils "mkdir"} -pv /mnt/Box";
+        serviceConfig.ExecStart = lib.concatStringsSep " " [
+            "${lib.getExe pkgs.rclone}"
+            "mount"
+            "--config" "/etc/rclone.conf"
+            "--log-level" "DEBUG"
+            "--log-systemd"
+            "--human-readable"
+            "--use-mmap"
+            "--cache-dir" "%T/rclone_box"
+            "--vfs-cache-mode" "full"
+            "--vfs-cache-max-size" "2G"
+            "--vfs-cache-max-age" "10m"
+            "--dir-cache-time" "2m"
+            "--poll-interval" "1m"
+            "--multi-thread-cutoff" "128M"
+            "--multi-thread-streams" "8"
+            "--umask" "022"
+            # "--disable-http2"
+            "--allow-other"
+            "Box: /mnt/Box"
+        ];
+        environment =
+            config.networking.proxy.envVars
+            // { GODEBUG = "netdns=go"; };
+        wantedBy = [ "multi-user.target" ];
+    };
 
 }
