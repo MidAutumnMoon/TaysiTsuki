39c1245d65582c51b506a1ebf56f4bc67caeb6e3
MidAutumnMoon <me@418.im>
2026-01-24 15:33:42 +0800

    home/niri: tweak

--- nixos/services/avahi/module.nix
+++ nixos/services/avahi/module.nix
@@ -19,7 +19,29 @@ in {
         };
 
     systemd.services.avahi-daemon = {
+        after = [ "network.target" ];
+        wants = [ "network.target" ];
+        # Conflict with RO etc overlay
         serviceConfig.ConfigurationDirectory = lib.mkForce [];
+    };
+
+    systemd.services.restart-avahi = {
+        description = "Restart Avahi Daemon";
+        serviceConfig.Type = "oneshot";
+        serviceConfig.ExecStart =
+            let ctl = lib.getExe' config.systemd.package "systemctl"; in
+            let avahi = config.systemd.services.avahi-daemon.name; in
+            "${ctl} restart ${avahi}";
+    };
+
+    systemd.timers.restart-avahi = {
+        description = "Hourly Timer for Avahi Restart";
+        timerConfig = {
+            OnCalendar = "hourly";
+            Persistent = true;
+            Unit = "restart-avahi.service";
+        };
+        wantedBy = [ "timers.target" ];
     };
 
 }
