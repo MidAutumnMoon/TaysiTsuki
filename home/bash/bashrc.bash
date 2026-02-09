1c1d2df4c9eb6ea4e94867968d3c73c6ebef3fc8
MidAutumnMoon <me@418.im>
2026-02-09 08:14:01 +0800

    home/bash: faster bashrc (feat. llm)

--- home/bash/bashrc.bash
+++ home/bash/bashrc.bash
@@ -1,29 +1,20 @@
-# Only launchs fish if it's interactive shell
-if [[ $- == *i* ]]; then
-    # Walk through parent processes up until PID 1
-    # to check if any of them is "fish". If such case
-    # is encountered, this Bash process is probably
-    # launched using nix shell or develop, in this case
-    # don't start a new fish shell.
+# Only launch fish if it's interactive shell
+if [[ $- == *i* && -z $BASH_EXECUTION_STRING ]]; then
+    # Walk through parent processes up to PID 1 to check if any is "fish".
+    # If found, this Bash was probably launched via nix shell/develop,
+    # so don't start a new fish shell (avoid nesting).
 
-    declare pid_on_hand="$$"
-    declare parents_have_fish="false"
+    pid=$$
+    while [[ "$pid" -ne 1 ]]; do
+        # Read directly from /proc - no process forks, much faster than ps
+        read -r cmd < "/proc/$pid/comm"
+        [[ "$cmd" == "fish" ]] && return
 
-    while [[ "$pid_on_hand" -ne 1 ]]; do
-        # Get the command name
-        cmd=$( ps -p "$pid_on_hand" -o comm= )
-
-        if [[ "$cmd" == "fish" ]]; then
-            parents_have_fish="true"
-            break
-        fi
-
-        parent_pid=$( ps -p "$pid_on_hand" -o ppid= )
-        # Remove whitespaces from the idiot output
-        pid_on_hand="${parent_pid//[[:space:]]}"
+        # Extract PPid from status file
+        while read -r key value; do
+            [[ "$key" == "PPid:" ]] && { pid=$value; break; }
+        done < "/proc/$pid/status"
     done
 
-    if [[ "$parents_have_fish" = "false" ]]; then
-        exec fish --interactive
-    fi
+    SHLVL=$((SHLVL - 1)) exec fish --interactive
 fi
