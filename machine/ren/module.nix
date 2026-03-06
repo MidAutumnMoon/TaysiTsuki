e40547d01701abad914d49086d5f6021efb6eb56
MidAutumnMoon <me@418.im>
2026-03-06 12:22:22 +0800

    ren: systemd sleep
    oops

--- machine/ren/module.nix
+++ machine/ren/module.nix
@@ -102,15 +102,6 @@
         enableRedistributableFirmware = true;
     };
 
-    # ren is a server which also happends to run desktop
-    # don't sleep
-    systemd.sleep.extraConfig = ''
-        AllowSuspend = no
-        AllowHibernation = no
-        AllowHybridSleep = no
-        AllowSuspendThenHibernate = no
-    '';
-
     nixpkgs.hostPlatform = "x86_64-linux";
 
 }
