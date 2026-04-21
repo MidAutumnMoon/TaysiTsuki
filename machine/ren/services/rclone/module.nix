6308de60546232285ca9eebe60d0188be30e19a0
MidAutumnMoon <me@418.im>
2026-04-21 10:50:17 +0800

    ren: try fix rclone

--- machine/ren/services/rclone/module.nix
+++ machine/ren/services/rclone/module.nix
@@ -13,30 +13,29 @@
         serviceConfig = {
             Type = "notify";
             RuntimeDirectory = "rclone";
+            ExecStartPre =
+                "${lib.getExe' pkgs.coreutils "mkdir"} -pv /mnt/Box";
+            ExecStart = lib.concatStringsSep " " [
+                "${lib.getExe pkgs.rclone}" "mount"
+                "--config" "/etc/rclone.conf"
+                "--log-level" "DEBUG"
+                "--log-systemd"
+                "--human-readable"
+                "--use-mmap"
+                "--cache-dir" "/tmp/rclone_box"
+                "--vfs-cache-mode" "full"
+                "--vfs-cache-max-size" "2G"
+                "--vfs-cache-max-age" "10m"
+                "--dir-cache-time" "2m"
+                "--poll-interval" "1m"
+                "--multi-thread-cutoff" "128M"
+                "--multi-thread-streams" "8"
+                "--umask" "022"
+                # "--disable-http2"
+                "--allow-other"
+                "Box:" "/mnt/Box"
+            ];
         };
-        serviceConfig.ExecStartPre =
-            "${lib.getExe' pkgs.coreutils "mkdir"} -pv /mnt/Box";
-        serviceConfig.ExecStart = lib.concatStringsSep " " [
-            "${lib.getExe pkgs.rclone}"
-            "mount"
-            "--config" "/etc/rclone.conf"
-            "--log-level" "DEBUG"
-            "--log-systemd"
-            "--human-readable"
-            "--use-mmap"
-            "--cache-dir" "%T/rclone_box"
-            "--vfs-cache-mode" "full"
-            "--vfs-cache-max-size" "2G"
-            "--vfs-cache-max-age" "10m"
-            "--dir-cache-time" "2m"
-            "--poll-interval" "1m"
-            "--multi-thread-cutoff" "128M"
-            "--multi-thread-streams" "8"
-            "--umask" "022"
-            # "--disable-http2"
-            "--allow-other"
-            "Box: /mnt/Box"
-        ];
         environment =
             config.networking.proxy.envVars
             // { GODEBUG = "netdns=go"; };
