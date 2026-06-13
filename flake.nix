b2ea495f1b2afaafaaf419a0e11824251120a1f8
MidAutumnMoon <me@418.im>
2026-06-13 18:50:44 +0800

    noctalia legacy v4

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
         # };
 
         noctalia = {
-            url = "github:noctalia-dev/noctalia-shell";
+            url = "github:noctalia-dev/noctalia-shell/legacy-v4";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
