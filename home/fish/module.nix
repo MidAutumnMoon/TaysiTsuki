f7b8cf05609390551f522c33a1419244fb5b2245
MidAutumnMoon <me@418.im>
2026-06-20 01:45:36 +0800

    home/fish: move __moonstep.fish into conf.d for clarity

--- home/fish/module.nix
+++ home/fish/module.nix
@@ -5,6 +5,7 @@
     packages = [ pkgs.fish ];
 
     xdg_config."fish/functions".src = dots.get "fish";
-    xdg_config."fish/conf.d/__moonstep.fish".src = dots.get "fish/__moonstep.fish";
+    xdg_config."fish/conf.d/__moonstep.fish".src =
+        dots.get "fish/conf.d/__moonstep.fish";
 
 }
