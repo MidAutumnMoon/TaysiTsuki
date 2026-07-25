912418819af2c4cb0765f1a523a3517935270856
MidAutumnMoon <me@418.im>
2026-07-25 17:59:40 +0800

    package: remove dirge

--- packages/tsuki.nix
+++ packages/tsuki.nix
@@ -170,12 +170,6 @@ allUnique <| withValidate <|
         attr = tsuki "mimic-cloud-init";
         group = gs.rust_1;
     }
-    # Not used for now, disable to save CI time
-    # {
-    #     attr = tsuki "dirge";
-    #     group = gs.rust_1;
-    #     update = {};
-    # }
     {
         attr = tsuki "inori";
         group = "Inori";
