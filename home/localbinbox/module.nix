92ff648571fc891d05111f317e556bf10117dc13
MidAutumnMoon <me@418.im>
2026-08-03 12:40:01 +0800

    home: fix build failure

--- home/localbinbox/module.nix
+++ home/localbinbox/module.nix
@@ -2,23 +2,13 @@
 { pkgs, dots, ... }:
 
 let
-
-    _7zzWith7zAlias = pkgs.symlinkJoin {
-        name = "7zz-with-7z-alias";
-        paths = [ pkgs._7zip-zstd ];
-        postBuild = ''
-            ln -s $out/bin/7zz $out/bin/7z
-        '';
-    };
-
     localbinbox = pkgs.callPackage ./. {};
-
 in
 
 {
 
     packages = with pkgs; [
-        _7zzWith7zAlias
+        _7zip-zstd
         jq
         localbinbox
     ];
