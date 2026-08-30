fc62a5c21b8bf65a8e16814f4bcef5245fe2e758
MidAutumnMoon <me@418.im>
2026-08-30 14:11:00 +0800

    home/noctalia: symlink settings.toml

--- home/noctalia/module.nix
+++ home/noctalia/module.nix
@@ -1,36 +1,9 @@
-# lny module — installs noctalia; symlinks repo home/noctalia (dir) -> $XDG_CONFIG_HOME/noctalia
-# also writes the noctalia systemd user service unit
-{ dots, pkgs, flakes, lib, ... }:
-
-let
-
-    noctalia = flakes
-        .noctalia.packages.${pkgs.stdenv.hostPlatform.system}
-        .default;
-in
+# symlinks repo home/noctalia/settings.toml to .local/state/noctalia/settings.toml; managed purely using Noctalia GUI
+{ dots, ... }:
 
 {
 
-    packages = [
-        # noctalia
-    ];
-
-    # xdg_config."noctalia".src = dots.get "noctalia";
-
-    # xdg_config."systemd/user/noctalia.service".text = ''
-    #     [Unit]
-    #     Description=Noctalia Shell Service
-    #     Requisite=graphical-session.target
-    #     After=graphical-session.target
-    #     ConditionEnvironment=WAYLAND_DISPLAY
-
-    #     [Service]
-    #     ExecStart=${lib.getExe noctalia}
-    #     Restart=on-failure
-    #     RestartSec=1
-
-    #     [Install]
-    #     WantedBy=graphical-session.target
-    # '';
+    home.".local/state/noctalia/settings.toml".src =
+        dots.get "noctalia/settings.toml";
 
 }
