f57165ebce2404174d42c88eb7872c162e11c929
MidAutumnMoon <me@418.im>
2026-09-03 18:37:34 +0800

    noctalia v5.0.1

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
         # };
 
         noctalia = {
-            url = "github:noctalia-dev/noctalia-shell/v5.0.0-beta.10";
+            url = "github:noctalia-dev/noctalia-shell/v5.0.1";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
