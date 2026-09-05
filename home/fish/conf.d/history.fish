a42eb5a0c3b63c1193cc14e90d1f0bd099b03cc1
MidAutumnMoon <me@418.im>
2026-09-06 01:14:52 +0800

    home: tweak fish bind

---
+++ home/fish/conf.d/history.fish
@@ -0,0 +1,6 @@
+bind alt-r history-prefix-search-backward
+
+# Remember to take more advantage of
+# - Ctrl+R (full history search)
+# - Alt+{Up,Down} (single "token" search)
+# - Up,Down (substring search, w/o pager)
